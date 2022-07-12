8e5d422130f6be6b83dcdd1620373636f2a219f4
Nathan Toombs <ntoombs19@gmail.com>
2022-07-12 15:56:05 -0500

    Fix index page to utilize full browser height regardless of content

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import { Container, Grid } from '@mui/material';
 import Header from '../src/components/header/header';
 import GenerationsGrid from '../src/components/generations/generationsGrid';
 import Dialog from '../src/components/details/dialog';
+import styles from '../src/styles/home.module.scss';
 
 export default function Home() {
     return (
@@ -21,9 +22,9 @@ export default function Home() {
             <Container maxWidth="xl">
                 <GenerationsGrid/>
             </Container>
-            <Container maxWidth="xl">
-                <Grid container spacing={'20px'}>
-                    <Grid style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundClip: 'content-box', marginBottom: '80px' }} sx={{ background: (theme) => theme.palette.background.cardHeader }} item xs={12} sm={6} md={4} lg={3}>
+            <Container className={styles.mainContent} maxWidth="xl">
+                <Grid className={styles.mainGrid} container spacing={'20px'}>
+                    <Grid className={styles.sidebar} sx={{ background: (theme) => theme.palette.background.cardHeader }} item xs={12} sm={6} md={4} lg={3}>
                         <h4 style={{ padding: '20px' }}>More filter options to be added here.</h4>
                     </Grid>
                     <Grid item xs={12} sm={6} md={8} lg={9}>
